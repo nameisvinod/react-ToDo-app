a3ce9ee133c03a8aa7718ffc07220ddbdf44d182
Vinod Kumar S <vinodkumar.s@zohocorp.com>
2020-06-26 23:59:23 +0530

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p> Todo List v.1.0.0. This is part of a react crash course.</p>
+        </React.Fragment>
+    )
+}
